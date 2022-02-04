e0642e5aef8bcf2cca1b8e48ef8c35adadcd1891
Nikolas Moya <moya@work.co>
2022-02-04 14:31:44 +0100

    Update script after many years

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 alvanista-exporter
 ==================
 
-Python application to export your public game list from http://alvanista.com
+Python3 application to export your public game list from http://alvanista.com
 
-### Dependencies
-- Python 2.7.*
+This may serve as backup or the input to another software that load your game library to another game tracker website
 
 ### Usage
 - Clone this repository
-- Go to the cloned folder and execute:
-```
-python app.py <alvanista_username> <finished|have|want>
-```
-- 'alvanista_username'.txt and 'alvanista_username'.json files will be create.
-- This may serve as backup or the input to another software that load your game library to another game tracker website. (aka. Backlogger)
+- Create a virtual environment `python3 -m venv venv`
+- Activate it: `source venv/bin/activate`
+- Install dependencies: `pip install -r requirements.txt`
+- Run: `python app.py <alvanista_username> <finished|have|want>`
+- `alvanista_username.txt` and `alvanista_username.json` files will be created
--- app.py
+++ app.py
@@ -1,81 +1,85 @@
-from bs4 import BeautifulSoup
-import urllib2
 import json
 import sys
+from collections import defaultdict
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
 
 
 def get_html_from_url(url):
-	response = urllib2.urlopen(url)
-	return response.read()
+    response = urlopen(url)
+    return response.read()
 
 
-def clean_plataform(plataform):
-	plataform = plataform.replace("-", " ")
-	if plataform == "ios":
-		return "iOS"
-	else:
-		plataform = plataform.lower()
-		tmp = []
-		for words in plataform.split():
-			tmp.append(words[0].upper() + words[1:])
-		return " ".join(tmp)
+def clean_platform(platform):
+    platform = platform.replace("-", " ")
+    if platform == "ios":
+        return "iOS"
+    else:
+        platform = platform.lower()
+        tmp = []
+        for word in platform.split():
+            tmp.append(word[0].upper() + word[1:])
+        return " ".join(tmp)
+
 
 def parse_games(games, a_links):
-	for tag in a_links:
-	    span = tag.find_all("span")
-	    plataform = tag['href'].split("/")[2]
-	    plataform = clean_plataform(plataform)
-	    for s in span:
-	        if plataform not in games:
-	        	games [plataform] = []
-	        games[plataform].append(s.text)
+    for tag in a_links:
+        span = tag.find_all("span")
+        platform = tag["href"].split("/")[2]
+        platform = clean_platform(platform)
+        for s in span:
+            games[platform].append(s.text)
+
 
 def dump_humans(games, username):
-	output = ''
-	keys = games.keys()
-	keys.sort()
-	for plataform in keys:
-		lst = games[plataform]
-		lst.sort()
-		output += "%s\n" % (plataform)
-		for game in lst:
-			output += "\t- %s\n" % (game)
-		output += "\n"
+    output = []
+    platforms = list(games.keys())
+    for platform in sorted(platforms):
+        output.append(f"{platform}\n")
+        for game in sorted(games[platform]):
+            output.append(f"\t- {game}")
 
-	output = output.encode("utf-8")
-	arq = open(username+".txt", "w")
-	arq.write(output)
-	arq.close()
-	print output
+    content = "\n".join(output)
+    save(f"{username}.txt", content)
+    return content
+
+
+def save(filename, content):
+    with open(filename, "w") as file:
+        file.write(content)
+
 
 def dump_json(games, username):
-	arq = open(username+".json", "w")
-	arq.write(json.dumps(games))
-	arq.close()
+    content = json.dumps(games)
+    save(f"{username}.json", content)
+    return content
+
 
 def main():
-	if len(sys.argv) != 3:
-		print "Call: python app.py <username> <category=finished|have|want>"
-		sys.exit(-1)
-	username = sys.argv[1]
-	category = sys.argv[2]
-	games = {}
-	page = 1
+    if len(sys.argv) != 3:
+        print("Call: python app.py <username> <category=finished|have|want>")
+        sys.exit(-1)
+    username = sys.argv[1]
+    category = sys.argv[2]
+    games = defaultdict(list)
+    page = 1
 
-	while True:
-		url = "http://alvanista.com/%s/games/%s?page=%d" % (username, category, page)
-		html = get_html_from_url(url)
-		soup = BeautifulSoup(html)
-		agames = soup.find_all("a", {"class":"game-cover-square game-cover-square-big"})
-		if len(agames) == 0:
-			break
-		else:
-			parse_games(games, agames)
-			page += 1
+    while True:
+        url = "http://alvanista.com/%s/games/%s?page=%d" % (username, category, page)
+        html = get_html_from_url(url)
+        soup = BeautifulSoup(html, features="html.parser")
+        all_games = soup.find_all("a", {"class": "game-cover-square game-cover-square-big"})
+        if len(all_games) == 0:
+            break
+        else:
+            parse_games(games, all_games)
+            page += 1
 
-	dump_humans(games, username)
-	dump_json(games, username)
+    content = dump_humans(games, username)
+    dump_json(games, username)
+    print(content)
 
 
 if __name__ == "__main__":
-	main()
+    main()
